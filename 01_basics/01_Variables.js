21d4576f0e972fd89b13f2ee70e850c9d93a6344
manishyoudumb <darksalterego99@gmail.com>
2024-06-25 15:11:02 +0530

    "Removed newline character at end of file"

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId = 223311
+
+let accountEmail = "manish@x.com"
+
+accountCity = "CTC"
+
+var accountPassword = "FuckyouMEAN"
+
+// accountId = 2 (This is not allowed)
+
+console.log(accountId);
+
+accountEmail = "Yuvi@maxarchxd.com"
+accountPassword = "WWWmmm"
+accountCity = "BBSR"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
