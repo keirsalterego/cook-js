c37eb51c6af7ad680c5b8b6bdce8e5baf879848f
manishyoudumb <darksalterego99@gmail.com>
2024-06-25 15:23:37 +0530

    feat: Add JavaScript data types examples

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3) // We arer using nodejs not browser 
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Manish")
+
+
+let name = "manish" // String datatype
+let age = 18 // number datatypes
+let isLoggedIn = false // Boolean Datatype
+let state;
+
+// number => 2 to power 53
+// bigint used speacially in stock market, big softwares
+// string => ""
+// boolean => true/false
+// null => standalone value >
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
