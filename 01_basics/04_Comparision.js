ad9540eca832786310f722a3ce893403ba3f3a64
manishyoudumb <darksalterego99@gmail.com>
2024-06-27 13:42:13 +0530

    feat: Add code for JavaScript comparison operators

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,49 @@
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 >= 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+console.log(2 === 1);
+console.log(2 !== 1);
+console.log(2 === "2");
+console.log(2 == "2");
+console.log(2 !== "2");
+
+// OUTPUT 
+
+// true
+// false
+// true
+// false
+// false
+// true
+// false
+// true
+// false
+// true
+// true
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null <= 0);
+console.log(null != 0);
+console.log(null === 0);
+console.log(null !== 0);
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(null !== undefined);
+
+// false
+// false
+// false
+// true
+// true
+// true
+// false
+// true
+// true
+// false
+// true
