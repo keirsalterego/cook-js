e7289ba71bb516afe8b7b9a4f446731ee6b999e5
manishyoudumb <darksalterego99@gmail.com>
2024-06-27 13:35:32 +0530

    chore: Refactor conversion operation code, add comments, and update data types examples

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -18,4 +18,27 @@ console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => False;
 
-let somenumber = 33
+let somenumber = 33 
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+
+// *************** Operations **************** //
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2); // Used in Cryptography called modulus
+console.log(2**2);
+
+
+let str1 ="Hola"
+let str2 = "Manish"
+let str3 = "str1+str2"
+console.log(str3);
+
