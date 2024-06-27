3b7a1ff84393f0b1ba4ad29020182cca7c7d50f6
manishyoudumb <darksalterego99@gmail.com>
2024-06-27 13:08:57 +0530

    chore: Refactor conversion operation code and add comments

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -6,3 +6,16 @@ console.log(typeof(score));
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => False;
+
+let somenumber = 33
