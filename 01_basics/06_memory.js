879b90909024dd8db47eede45241daec7f0837ca
manish <darksalterego99@gmail.com>
2025-02-10 22:26:29 +0530

    Added comprehensive datatypes summary and memory management examples

---
+++ 01_basics/06_memory.js
@@ -0,0 +1,18 @@
+// +++++++++++++++++++++++++++++++
+
+// Stack and Heap
+// Stack: Primitive data types
+// Heap: Reference data types (Non-Primitives)
+
+let myname = "manish"
+
+let anotherName = myname
+
+console.log(anotherName)
+
+
+// Stack
+
+
+
+
