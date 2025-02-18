5db648c2f9efacaa01e861c626420d16896ffb5a
manish <darksalterego99@gmail.com>
2025-02-18 15:04:56 +0530

    Added additional date methods and examples in JavaScript

--- 01_basics/09_dates.js
+++ 01_basics/09_dates.js
@@ -1,5 +1,15 @@
 // Dates
 
-let myDateDate = new Date();
+let myDate = new Date();
 console.log(myDate); // 2021-09-19T06:00:00.000Z
 
+console.log(myDate.toString()); // Mon Sep 20 2021 11:30:00 GMT+0530 (India Standard Time)
+console.log(myDate.toDateString()); // Mon Sep 20 2021
+
+console.log(typeof myDate); // object
+
+let myCreatedDate = new Date('2021-09-20');
+console.log(myCreatedDate); // 2021-09-19T18:30:00.000Z
+console.log(myDate.toLocaleString());
+
+console.log(myDate.getFullYear()); // 2021
